328ab30c4142be5c0e05e42584806dcebc8942d2
Adson Martins <cookiezaumbolado@gmail.com>
2020-05-11 23:55:12 -0300

    localStorage simple function

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { GlobalStyle } from "../../components/styles";
 
 export default function Home() {
-  const [theme, _setTheme] = useState("light");
+  const [theme, _setTheme] = useState(localStorage.getItem("theme"));
 
   const getBackground = theme === "dark" ? "#1c1a1a" : "#FFF";
   const getForeground = theme === "dark" ? "#FFF" : "#1c1a1a";
+
+  useEffect(() => {
+    if (!localStorage.getItem("theme")) localStorage.setItem("theme", "light");
+  });
+
+  function handleChangeTheme() {
+    if (theme === "dark") {
+      _setTheme("light");
+      localStorage.setItem("theme", "light");
+    } else {
+      _setTheme("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  }
 
   return (
     <div>
@@ -15,13 +29,7 @@ export default function Home() {
         foregroundColor={getForeground}
       />
       <h1>Theme Selector (Light and Dark Theme)</h1>
-      <button
-        onClick={() =>
-          theme === "dark" ? _setTheme("light") : _setTheme("dark")
-        }
-      >
-        Change Color
-      </button>
+      <button onClick={() => handleChangeTheme()}>Change Color</button>
     </div>
   );
 }
